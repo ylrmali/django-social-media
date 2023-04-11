d4f140acae8cb83a4cdcdaeeab887d80b12a2d35
ylrmali <ylrmali1289@gmail.com>
2023-04-11 13:57:14 +0300

    debug true

--- social/settings.py
+++ social/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jz@4so45_u3&&t755n*q@+#!_5m4kl)9c2l7cx*91z4l8)k3!h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
