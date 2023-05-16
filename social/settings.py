800a56ea21e2d943ee87852931f10d9a55cfc97e
Ali Yıldırım <ylrmali1289@gmail.com>
2023-05-16 23:39:18 +0300

    activated postgresql

--- social/settings.py
+++ social/settings.py
@@ -91,25 +91,23 @@ SITE_ID = 1
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
-}
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': BASE_DIR / 'db.sqlite3',
+#      }
+# }
 
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'socialmedia',
-        'USER': 'postgres',
+        'NAME': 'social',
+        'USER': 'ali',
         'PASSWORD': '060489a+',
         'HOST': '127.0.0.1',
         'PORT': 5432,
     }
 }
-'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -161,3 +159,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import django_heroku
 django_heroku.settings(locals())  # share my setting with gunicorn
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
