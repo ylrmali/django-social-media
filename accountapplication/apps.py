ac64fed85cbc737f97e8688734da84dabe391acf
Ali Yıldırım <ylrmali1289@gmail.com>
2023-05-16 23:35:30 +0300

    imported signals function

--- accountapplication/apps.py
+++ accountapplication/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class AccountapplicationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accountapplication'
+
+    ''' we import under the ready function because after the import signal, 
+        all application should be install '''
+    def ready(self):
+        import accountapplication.signals
