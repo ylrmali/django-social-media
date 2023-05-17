96e1304b4e977043b2ebd81bbc9618f66cffa24e
Ali Yıldırım <ylrmali1289@gmail.com>
2023-05-17 21:13:32 +0300

    added try except block for profile function

--- accountapplication/views.py
+++ accountapplication/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from accountapplication.models import User, Post, Profile
+
 
 # Create your views here.
 
@@ -20,25 +21,34 @@ def index(request):
 
 
 # user profile page
-def profile(request, username):
-    target_user = Profile.objects.get(user=username)   # target user / who's profile will load
-    id = Profile.objects.get(user=username).id         # target user id
-    posts = Post.objects.filter(owner=id)               # target user's posts
-    post_count = posts.count()                          # target user's post count
-    follower_count = target_user.follower.count()       # target user's follower count
-    following_count = target_user.following.count()     # target user's following count
-    # is the current user following the target user?
-    tu_is_follower = target_user.follower.filter(username=request.user).exists()  # is current user a follower?
-    context = {
-        'user_data': target_user,
-        'user_post': posts,
-        'post_count': post_count,
-        'follower_count': follower_count,
-        'following_count': following_count,
-        'user_id': id,
-        'is_follower': tu_is_follower,
-    }
-    return render(request, 'accountapplication/profile.html', context)
+def profile(request, user_id):
+    try:
+        user = User.objects.get(id=user_id)
+        target_user = Profile.objects.get(user=user_id)   # target user / who's profile will load
+        id = Profile.objects.get(user=user_id).id       # target user id
+        posts = Post.objects.filter(owner=id)               # target user's posts
+        post_count = posts.count()                          # target user's post count
+        follower_count = target_user.follower.count()       # target user's follower count
+        following_count = target_user.following.count()     # target user's following count
+        # is the current user following the target user?
+        tu_is_follower = target_user.follower.filter(user=request.user).exists()  # is current user a follower?
+
+        context = {
+            'user_data': user,
+            'profile_data': target_user,
+            'user_post': posts,
+            'post_count': post_count,
+            'follower_count': follower_count,
+            'following_count': following_count,
+            'user_id': id,
+            'is_follower': tu_is_follower,    
+        }
+
+        return render(request, 'accountapplication/profile.html', context)
+
+    except (Profile.DoesNotExist) as err:
+        return HttpResponse('Error' + err); 
+    
 
 
 def get_follower(request, username):
