ccb70666b751a8aa9434ccf621705187cef733ce
Ali Yıldırım <ylrmali1289@gmail.com>
2023-05-17 21:09:10 +0300

    Profile models added admin site

--- accountapplication/admin.py
+++ accountapplication/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Post, User, Comment
+from .models import Post, User, Comment, Profile
 
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(User)
 admin.site.register(Comment)
+admin.site.register(Profile)
 
