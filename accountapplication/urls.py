9d86cfa185d0affead845fbb27d1750f1e19bed3
Ali Yıldırım <ylrmali1289@gmail.com>
2023-05-17 21:10:42 +0300

    fixed profile url params issue

--- accountapplication/urls.py
+++ accountapplication/urls.py
@@ -3,7 +3,7 @@ from accountapplication import views
 
 urlpatterns = [
     path('home/', views.index, name='home'),
-    path('<str:username>/', views.profile, name='profile'),
+    path('<str:user_id>/', views.profile, name='profile'),
     path('<str:username>/follower/', views.get_follower, name='get-follower'),
     path('<str:username>/following/', views.get_following, name='get-following'),
 ]
