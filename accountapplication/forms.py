621a938e39ef29d03d665ded57a6d46849d2a0c9
Ali Yıldırım <ylrmali1289@gmail.com>
2023-06-11 15:48:26 +0300

    added profile edit form

--- accountapplication/forms.py
+++ accountapplication/forms.py
@@ -1,1 +1,12 @@
 from django import forms
+from accountapplication.models import Profile
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["profile_photo", "background_photo"]
+        labels = {
+            "profile_photo": "Profil Fotoğrafı",
+            "background_photo": "Arkaplan Fotoğrafı",
+        }
