712a2174b08e0dec1fa21fb0baac8cec7304578f
Ali Yıldırım <ylrmali1289@gmail.com>
2023-06-12 20:37:26 +0300

    created like button color event

--- mainapp/static/mainapp/js/post-detail.js
+++ mainapp/static/mainapp/js/post-detail.js
@@ -1,7 +1,19 @@
-const likeCount = document.getElementById('likeCount');
+//! first wait for load like button
+setTimeout(()=> {
 
-likeCount.addEventListener('click',()=>{
-    setInterval(function (){
-
-    },10);
-});
+    const likeIcon = document.getElementById('likeIcon');
+    
+    likeIcon.addEventListener('click',() => {
+        const likeControl = likeIcon.getAttribute('data-like-control');
+        console.log(likeControl)
+        if (likeControl === 'True') {//#e73c04
+            likeIcon.setAttribute('data-like-control', 'False');
+            likeIcon.style.color = 'white';
+        }
+        else{
+            console.log('asd')
+            likeIcon.setAttribute('data-like-control', 'True');
+            likeIcon.style.color = '#e73c04';
+        }
+    });
+}, 1000);
