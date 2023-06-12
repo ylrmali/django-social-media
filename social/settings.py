f0fba22d711beb4bfc883b59cf0f3fa7990567f7
Ali Yıldırım <ylrmali1289@gmail.com>
2023-06-12 09:48:05 +0300

    media root and url settings fixed

--- social/settings.py
+++ social/settings.py
@@ -148,8 +148,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-MEDIA_ROOT = BASE_DIR / "uploads"
-MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
 
 
 # Default primary key field type
