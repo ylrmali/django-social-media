4f318b5ec680ebe323e1b848572de896d4b53fd6
Ali Yıldırım <ylrmali1289@gmail.com>
2023-06-16 17:04:08 +0300

    a

--- accountapplication/signals.py
+++ accountapplication/signals.py
@@ -3,7 +3,7 @@ from accountapplication.models import User, Profile
 from django.dispatch import receiver
 
 
-''' When the user create an account, profile automaticly will be creat '''
+''' When the user create an account, profile automaticly will be create '''
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
