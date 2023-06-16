34276b89ea4fcca06ea9dda32749e9426369883a
Ali Yıldırım <ylrmali1289@gmail.com>
2023-06-16 15:09:13 +0300

    created notification delete func & change state

--- accountapplication/static/accountapplication/js/navbar.js
+++ accountapplication/static/accountapplication/js/navbar.js
@@ -10,3 +10,27 @@ search.addEventListener('click', ()=>{
 function closeSearch(){
     searchBar.style.display = 'none';
 }
+
+/* open notifications */
+const notification = document.getElementById('notification-menu');
+const actions = document.getElementById('actions');
+
+actions.addEventListener('click', () => {
+    notification.style.display = 'block';
+});
+
+
+/* notification */
+function deleteNotification(element){
+    element.remove()
+};
+
+function changeState(thisElement, targetElement){
+    thisElement.innerHTML = `
+        <div class="row" id='read'>
+            <a class="re_follow_button"
+                hx-get="{% url 'mainapp:refollow' current_user_id=noti.user.id target_user_id=noti.owner_user.id noti_id=noti.id %}">
+                Sende Takip Et
+            </a>
+        </div>`;
+};
